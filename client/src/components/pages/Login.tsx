0baada7a08b77e4684e00fceb6ee2d5b0db81620
HobolandJacob <xxpwnage37xx@gmail.com>
2021-02-03 12:38:47 -0600

    Login UI Cleanup and Fix

--- client/src/components/pages/Login.tsx
+++ client/src/components/pages/Login.tsx
@@ -5,11 +5,12 @@ import {
   FormLabel,
   Heading,
   Input,
+  theme,
+  useColorMode,
 } from "@chakra-ui/react";
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { API_DOMAIN } from "../../util/consts";
-import Layout from "./Layout";
 
 interface ILogin {
   username: string;
@@ -60,60 +61,94 @@ export default class Login extends Component<{}, ILogin> {
       });
   };
 
+  submit = async (event: React.FormEvent) => {
+    let { username, password } = this.state;
+    // Willingly Ignoring Validation...
+    this.setState({ loading: true });
+    await fetch(`${API_DOMAIN}/user/login`, {
+      credentials: "include",
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) this.setState({ auth: true });
+        else this.setState({ loading: false });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    event.preventDefault();
+  };
+
   render() {
     if (this.state.auth) return <Redirect to="/" />;
     return (
-      <Layout>
-        <Box
-          bgColor="white"
-          marginLeft={["5%", "5%", "10%", "20%"]}
-          marginRight={["5%", "5%", "10%", "20%"]}
-          borderRadius={3}
-          boxShadow="dark-lg"
-          marginTop="15%"
-          minHeight="100%"
-          padding={8}
-          overflow="hidden"
+      <LoginLayout submit={this.submit}>
+        <Heading textAlign="center" color="black" marginBottom={10}>
+          ATAS Login
+        </Heading>
+        <FormControl marginTop={3} id="username" isRequired>
+          <FormLabel color="black">Username</FormLabel>
+          <Input
+            color="black"
+            isRequired
+            autoComplete="username"
+            readOnly={this.state.loading}
+            placeholder="username"
+            value={this.state.username}
+            onChange={(event) =>
+              this.setState({ username: event.target.value })
+            }
+          />
+        </FormControl>
+        <FormControl marginTop={3} marginBottom={3} id="password" isRequired>
+          <FormLabel color="black">Password</FormLabel>
+          <Input
+            color="black"
+            autoComplete="current-password"
+            readOnly={this.state.loading}
+            placeholder="password"
+            value={this.state.password}
+            onChange={(event) =>
+              this.setState({ password: event.target.value })
+            }
+            type="password"
+            isRequired
+          />
+        </FormControl>
+        <Button
+          value="solid"
+          color="black"
+          type="submit"
+          isLoading={this.state.loading}
         >
-          <Heading textAlign="center" marginTop={5} marginBottom={10}>
-            ATAS Login
-          </Heading>
-          <FormControl marginTop={3} id="username" isRequired>
-            <FormLabel>Username</FormLabel>
-            <Input
-              autoComplete="username"
-              readOnly={this.state.loading}
-              placeholder="username"
-              value={this.state.username}
-              onChange={(event) =>
-                this.setState({ username: event.target.value })
-              }
-            />
-          </FormControl>
-          <FormControl marginTop={3} id="password" isRequired>
-            <FormLabel>Password</FormLabel>
-            <Input
-              autoComplete="current-password"
-              readOnly={this.state.loading}
-              placeholder="password"
-              value={this.state.password}
-              onChange={(event) =>
-                this.setState({ password: event.target.value })
-              }
-            />
-          </FormControl>
-          <Button
-            type="submit"
-            isLoading={this.state.loading}
-            float="right"
-            marginTop={3}
-            colorScheme="blue"
-            onClick={this.login}
-          >
-            Login
-          </Button>
-        </Box>
-      </Layout>
+          Login
+        </Button>
+      </LoginLayout>
     );
   }
 }
+
+const LoginLayout: React.FC<{
+  submit: (event: React.FormEvent) => Promise<void>;
+}> = ({ children, submit }) => {
+  const { colorMode } = useColorMode();
+  return (
+    <Box
+      position="fixed"
+      top="50%"
+      left="50%"
+      marginRight="-50%"
+      transform="translate(-50%, -50%)"
+      backgroundColor={colorMode === "light" ? "white" : theme.colors.gray[200]}
+      borderRadius={3}
+      boxShadow="lg"
+      padding={[5, 10]}
+    >
+      <form onSubmit={submit}>{children}</form>
+    </Box>
+  );
+};
