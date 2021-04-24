f17e8f7868b10146bf780b2c084ffff59dfa37b9
HobolandJacob <xxpwnage37xx@gmail.com>
2021-04-23 19:42:38 -0500

    Admin page UI rework

--- client/pages/admin.tsx
+++ client/pages/admin.tsx
@@ -1,40 +1,39 @@
-import { Box } from "@chakra-ui/layout";
 import { useRouter } from "next/router";
-import { FunctionComponent, useContext, useEffect } from "react";
+import React, { FunctionComponent, useEffect } from "react";
 import EntryFilter from "../components/entry/EntryFilter";
 import EntryModal from "../components/entry/EntryModal";
 import EntryTable from "../components/entry/EntryTable";
-import { EntryContext } from "../context/EntryContext";
+import Layout from "../components/Layout";
+import Navbar from "../components/navbar/Navbar";
+import EntryContextProvider from "../context/EntryContext";
 import IRole from "../interface/IRole";
 import { useUser } from "../util/swrFunctions";
 
 const Admin: FunctionComponent = () => {
-  const context = useContext(EntryContext);
-  const { data, loading } = useUser();
   const router = useRouter();
+  const { data, loading } = useUser();
 
   useEffect(() => {
-    if (!data) {
+    if (!loading && data === undefined) {
       router.push("/login");
-      return;
     }
 
-    if (data.role !== IRole.ADMIN) {
-      router.push("/login");
-      return;
-    }
+    if (data.role !== IRole.ADMIN) router.push("/");
   }, [data, loading]);
 
-  if (loading) {
-    return <></>;
-  }
+  if (loading || !data) return <></>;
 
   return (
-    <Box padding="1%" paddingLeft="10%" paddingRight="10%">
-      <EntryFilter />
-      <EntryTable />
-      <EntryModal />
-    </Box>
+    <>
+      <Navbar />
+      <EntryContextProvider>
+        <Layout>
+          <EntryFilter />
+          <EntryTable />
+          <EntryModal />
+        </Layout>
+      </EntryContextProvider>
+    </>
   );
 };
 
