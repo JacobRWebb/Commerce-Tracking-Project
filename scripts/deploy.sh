b6ff4b16dc086b8802a7d78dcccef406833ce640
JacobRWebb <xxpwnage37xx@gmail.com>
2021-08-09 12:10:48 -0500

    Potential hang fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-ssh -tt root@xodius.io <<EOF
+ssh -tt root@xodius.io <<-'ENDSSH'
   cd ~/Commerce-Tracking-Project
   git stash push --include-untracked
   git pull
   yarn setup
-EOF
->>
+ENDSSH
