690e1d06d0edf943d539d0f1e8718cf6ffd108c3
JacobRWebb <xxpwnage37xx@gmail.com>
2021-08-10 11:45:42 -0500

    Secured socket

--- client/util/constants.ts
+++ client/util/constants.ts
@@ -1,9 +1,7 @@
-export const API_DOMAIN =
-  process.env.NODE_ENV === "production"
-    ? "https://api.xodius.io"
-    : "http://localhost:5050";
+export const isProd = process.env.NODE_ENV === "production";
 
-export const WS_DOMAIN =
-  process.env.NODE_ENV === "production"
-    ? "ws://api.xodius.io"
-    : "ws://localhost:5050";
+export const API_DOMAIN = isProd
+  ? "https://api.xodius.io"
+  : "http://localhost:5050";
+
+export const WS_DOMAIN = isProd ? "wss://api.xodius.io" : "ws://localhost:5050";
