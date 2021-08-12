50ed0dc716729d336e966733fc3e30ff9f8e5561
JacobRWebb <xxpwnage37xx@gmail.com>
2021-08-12 10:54:03 -0500

    Fixed logout

--- client/pages/logout.tsx
+++ client/pages/logout.tsx
@@ -1,22 +1,33 @@
 import { serialize } from "cookie";
-import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+import { actionCreators, wrapper } from "features";
 import { FunctionComponent } from "react";
 
 const Logout: FunctionComponent = () => {
   return <div></div>;
 };
 
-export const getServerSideProps = ({
-  req,
-  res,
-}: GetServerSidePropsContext): GetServerSidePropsResult<{}> => {
-  res.setHeader("Set-Cookie", serialize("token", "", { path: "/", maxAge: 0 }));
-  return {
-    redirect: {
-      destination: "/login",
-      permanent: false,
-    },
-  };
-};
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) =>
+    async ({ req, res }) => {
+      res.setHeader(
+        "Set-Cookie",
+        serialize("token", "", {
+          path: "/",
+          maxAge: 0,
+          domain: "xodius.io",
+          secure: true,
+        })
+      );
+
+      await store.dispatch(actionCreators.AuthActions.logout());
+
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    }
+);
 
 export default Logout;
